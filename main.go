0e915203335938720c8e852d13c7c846154048a9
Ruben <ruben.lopez.deleon@gmail.com>
2024-04-25 16:36:48 -0300

    añadiendo comentarios

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var v = 4
+	var v = 4 // declaracion de una variable
+	//Mostrammos una variable y su direccion de memoria usando el caracter &
 	fmt.Printf("el valor de la variable v es %v y su direccion de memoria es %v\n", v, &v)
 }
