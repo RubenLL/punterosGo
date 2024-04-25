532cd004cb0aafb49668f17c9c01b87bc87ea796
Ruben <ruben.lopez.deleon@gmail.com>
2024-04-25 16:30:02 -0300

    creamos una variable y visualizamos el valor que contiene y la direccion de memoria donde se almacena

---
+++ go.mod
@@ -0,0 +1,3 @@
+module punterosteste
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var v = 4
+	fmt.Printf("el valor de la variable v es %v y su direccion de memoria es %v\n", v, &v)
+}
