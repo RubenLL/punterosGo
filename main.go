ebb4a1bc99f61db3b7994d45b682d6430f5b0235
Ruben <ruben.lopez.deleon@gmail.com>
2024-04-26 10:03:06 -0300

    añadiendo punteros y sus valores

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var v = 4 // declaracion de una variable
+	var v = 4  // declaracion de una variable
+	var p *int //Declaracion de variable
+	p = &v     //atribuir a direccion de memoria de la variable v
 	//Mostrammos una variable y su direccion de memoria usando el caracter &
 	fmt.Printf("el valor de la variable v es %v y su direccion de memoria es %v\n", v, &v)
+	//Mostrammos el valor  de la  variable usando el * y su direccion de memoria como es un puntero no necesitamos ningun caracter
+	fmt.Printf("el valor de la variable p es %v y su direccion de memoria es %v\n", *p, p)
 }
