02316c9f3619e383edfe9801a6ba6e11fe2a5ea7
Ruben <ruben.lopez.deleon@gmail.com>
2024-04-26 15:46:05 -0300

    usando una lista enlazada

--- main.go
+++ main.go
@@ -22,6 +22,36 @@ func main() {
 	byRef(&v)
 	fmt.Printf("El valor fuera de la funcion byValue es %v\n", v)
 
+	// vamos a crear un flujo donde vamos a inserir clientes dentro de uuna lista enlazada
+	var firstClient, lastClient *Client
+
+	var name, phoneNumber, continuar string
+	keep := true
+	for keep {
+
+		fmt.Println("Cual es el nombre del Cliente: ")
+		fmt.Scan(&name)
+		fmt.Println("Cual es el telefono del Cliente: ")
+		fmt.Scan(&phoneNumber)
+		var newCustomer Client
+		newCustomer.Name = name
+		newCustomer.PhoneNumber = phoneNumber
+		if firstClient == nil { //Es el primerCliente
+			firstClient = &newCustomer
+			lastClient = firstClient
+		} else {
+			lastClient.NextCustomer = &newCustomer
+			lastClient = &newCustomer
+		}
+		fmt.Println("Desea inserir otro Cliente: ")
+		fmt.Scan(&continuar)
+		if continuar == "no" {
+			keep = false
+		}
+
+	}
+	listarClientes(firstClient)
+
 }
 
 func byValue(num int) {
@@ -31,4 +61,29 @@ func byValue(num int) {
 func byRef(num *int) {
 	*num = *num + 1
 	fmt.Printf("El valor dentro de la funcion byRef es %v\n", *num)
+}
+
+// Ejemplo de punteros usando una structura que sera usada como una lista enlazada
+type Client struct {
+	Name         string `json:"Name"`
+	PhoneNumber  string `json:"PhoneNumber"`
+	NextCustomer *Client
+}
+
+func listarClientes(inicio *Client) {
+	fmt.Println("Presentando la lista de clientes: ")
+	currentClient := inicio
+	if currentClient == nil {
+		return
+	}
+	for {
+		fmt.Printf("Nombre: %v \n", currentClient.Name)
+		fmt.Printf("Telefono: %v \n", currentClient.PhoneNumber)
+		currentClient = currentClient.NextCustomer
+		if currentClient.NextCustomer == nil {
+			fmt.Printf("Nombre: %v \n", currentClient.Name)
+			fmt.Printf("Telefono: %v \n", currentClient.PhoneNumber)
+			return
+		}
+	}
 }
